edf6da15f810e24e89908d37185a63d1fefcce3e
Saurabh <257531276+sudosammi@users.noreply.github.com>
2026-02-07 10:04:37 +0530

    Day 5: Python Variable and User Interaction
    Aaj maine Python programming ke core concepts seekhe:
    Data Types: String (Text), Integer (Number), aur Boolean (True/False) ka difference samjha.
    User Input: input() function use karke script ko interactive banaya.
    Concatenation: Strings aur variables ko jodkar dynamic output generate kiya.
    Files: variables.py (Data types check) aur interaction.py (User se baat karne wala tool).

---
+++ interaction.py
@@ -0,0 +1,13 @@
+# Day 5: User Interaction
+print("--- STOP! SECURITY CHECK ---")
+
+# 1. Computer sawal puchega (Input)
+codename = input("Apna Hacker Name batao: ")
+
+# 2. Dusra sawal
+target = input("Aaj kisko hack karna hai?: ")
+
+# 3. Jawab (Output) using Variables
+print("Access Granted to: " + codename)
+print("Target Locked on: " + target)
+print("Starting attack on " + target + "...")
---
+++ variables.py
@@ -0,0 +1,20 @@
+# Day 5: Variables Mission
+
+# 1. String (Text wala data)
+name = "Saurabh"
+
+# 2. Integer (Number wala data)
+day = 5
+
+# 3. Boolean (Sach ya Jhoot)
+is_hacker = True
+
+# Ab result print karte hain
+print("Hacker Name:", name)
+print("Current Day:", day)
+print("Is he a hacker?:", is_hacker)
+
+# Ab Data Type (Sticker) check karte hain
+print(type(name))
+print(type(day))
+print(type(is_hacker))
