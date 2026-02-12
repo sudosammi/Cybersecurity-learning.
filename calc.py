abeb6da139521a4b6187a797a1978029b46e4453
Saurabh <257531276+sudosammi@users.noreply.github.com>
2026-02-12 13:32:49 +0530

    Day 10 : Function & Modular Coding
    Function
    .Parameters
    .return

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add_num(num1, num2):
+
+    total = num1 + num2
+    return total
+
+a = int(input("Enter your First Num :"))
+b = int(input("Enter your second Num :"))
+ 
+total = add_num(a, b)
+
+print("Sum Is :", total)
---
+++ my_tools.py
@@ -0,0 +1,16 @@
+#DAY 10: My First tool
+
+def hacker_greeting():
+
+    print("-----------------------")
+    print(     "ACCESS GRANTED"    )
+    print("Welcome bsck, sudosammi")
+
+    print("------------------------")
+
+hacker_greeting()
+
+print(" Kuch kaam karta hai")
+
+hacker_greeting()
+
